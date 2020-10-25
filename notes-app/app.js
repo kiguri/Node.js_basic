776fff3480053e6af7bc847f187274554735a633
HuyHQ <huyha14082012@gmail.com>
2020-10-25 13:10:46 +0700

    notes app

---
+++ notes-app/app.js
@@ -0,0 +1,76 @@
+const yargs = require('yargs');
+const notes = require('./notes');
+
+//Customize yargs version
+yargs.version('1.1.0');
+
+//Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    //tham số của command
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.addNote(argv.title, argv.body)
+    }
+});
+
+//Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.removeNote(argv.title)
+    }
+})
+
+//Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List all note',
+    handler() {
+        notes.listNotes();
+    }
+})
+
+//Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.readNote(argv.title);
+    }
+})
+
+
+
+
+
+
+
+
